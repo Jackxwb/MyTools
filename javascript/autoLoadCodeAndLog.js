4e4e3259afbae447fdb44ad4042b2dbd77ee551c
Jackxwb <34720403+Jackxwb@users.noreply.github.com>
2024-05-05 21:36:54 +0800

    JSON翻译辅助工具v2 如意长长长 v0.2

---
+++ javascript/autoLoadCodeAndLog.js
@@ -0,0 +1,19 @@
+document.getElementById("code").innerText = code.toString();
+document.getElementById("updateLog").innerHTML="";
+update.forEach((str)=>{
+    let li = document.createElement("li");
+    li.innerText = str;
+    document.getElementById("updateLog").appendChild(li);
+})
+
+function smail() {
+    let str = code.toString();
+    str = str.replaceAll(/\n[ ]*/g, "")
+    str = str.replaceAll(/^[^\{]*\{/g, "javascript:")
+    str = str.replaceAll(/\}$/g, "")
+    let a = document.getElementById("fast");
+    a.innerText = scriptName;
+    a.href = str;
+    console.log(str)
+}
+smail()
